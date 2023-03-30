1e10b6e164f2fa39b9370f5ea9584d6e444e509c
ljh010110 <ljh010110@g.skku.edu>
2023-03-30 20:15:52 +0900

    sum_of_squares_of_even

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
 from typing import List
+
 # Skeleton code for even_list
 def even_list(int_list: List[int]) -> List[int]:
-"""
-Determines if a number is even and return an even list.
-Args:
-int_list: A list of integer.
-Returns:
-A list of even integers.
-"""
-# TODO: Implement even_list
-pass
+    """
+    Determines if a number is even and return an even list.
+    Args:
+    int_list: A list of integer.
+    Returns:
+    A list of even integers.
+    """
+    # TODO: Implement even_list
+    pass
+
 # Skeleton code for sum_of_squares_of_even
 def sum_of_squares_of_even(even_int_list):
-"""
-Computes the sum of the squares of all even numbers in a list of integers.
-Args:
-even_int_list: A list of even integers.
-Returns:
-The sum of the squares of all even numbers in the list.
-"""
-add = 0
-for i in range(even_int_list):
-    add += even_int_list[i]^2
-return add
+    """
+    Computes the sum of the squares of all even numbers in a list of integers.
+    Args:
+    even_int_list: A list of even integers.
+    Returns:
+    The sum of the squares of all even numbers in the list.
+    """
+    def sum_of_squares_of_even(even_int_list):
+        add = 0
+        for i in even_int_list:
+            a = i
+            add += a*a 
+        return add
     
 # Main function
 def main():
-# Example list
-int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-even_int_list = even_list(int_list)
-output = sum_of_squares_of_even(even_int_list)
-print(output)
+    # Example list
+    int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    even_int_list = even_list(int_list)
+    output = sum_of_squares_of_even(even_int_list)
+    print(output)
+
 # Boilerplate code
 if __name__ == "__main__":
-main()
+    main()
